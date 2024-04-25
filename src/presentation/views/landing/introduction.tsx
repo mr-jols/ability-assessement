9480a480cf4623a0ad2e026ed4a59e2ede4f0832
Jola H Batin <jolawepiss@gmail.com>
2024-04-25 17:48:28 +0100

    added searchbar to introduction

--- src/presentation/views/landing/introduction.tsx
+++ src/presentation/views/landing/introduction.tsx
@@ -1,7 +1,7 @@
 export default function Introduction() {
   return (
     <div className="givelayout py-[2rem] tab:py-[3rem]">
-      <div className="text-center rounded-[1rem] py-[2rem] tab:py-[4rem] bg-[#0066F5] text-white px-[16px]">
+      <div className="text-center rounded-[1rem] py-[2rem] tab:py-[4rem] bg-[#0066F5] text-white px-[1rem]">
         <div className="tab:max-w-[60%] laptop:max-w-[40%] mx-auto ">
           <h1 className="text-[2rem] tab:text-[50px] font-[900] leading-[100%]">
             Ability Assess Assessment Platform
@@ -11,6 +11,17 @@ export default function Introduction() {
         assessments, ensuring personalized and effective support for unique
         needs`}
           </p>
+
+          <div className="relative flex items-center mx-auto mt-[2rem]">
+            <input
+              type="text"
+              className="rounded-[0.5rem] py-[1rem] w-full px-[1rem]"
+              placeholder="Search for an assessment"
+            />
+            <button className="bg-[#0066F5] rounded-[0.5rem] px-[2rem] py-[0.6rem] absolute right-[0.5rem]">
+                Search
+            </button>
+          </div>
         </div>
       </div>
     </div>
